f8c2e93475c600dd71c5758d497aff7a3abc454d
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2019-01-24 14:50:17 +0800

    Adds a basic idea of generators

---
+++ src/app/index.js
@@ -0,0 +1,34 @@
+import Generator from 'yeoman-generator';
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+import questions from './questions.js';
+import localGenerator from './generator';
+
+export default class LegacyDeploymentGenerator extends Generator {
+    constructor(args, opts) {
+        super(args, opts);
+
+        // This makes `gen-path` a required argument.
+        // this.argument('gen_path', {
+        //     type: String,
+        //     required: true,
+        //     desc: 'A generation path for promised component'
+        // });
+
+        // fs.accessSync(this.options.gen_path, fs.constants.R_OK | fs.constants.W_OK);
+    }
+
+    prompting() {
+        // return this.prompt(questions)
+        //     .then((answers) => {
+        //         var gen = new localGenerator(this.options.gen_path, answers);
+        //         var logs = gen.startGeneration();
+        //         if (logs.length > 0) {
+        //             for (var i = 0; i < logs.length; i++) {
+        //                 this.log(logs[i]);
+        //             }
+        //         }
+        //     });
+    }
+};
