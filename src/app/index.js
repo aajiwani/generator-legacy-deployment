e66a7a3ef7f93d21ca928565bcd58073e6f914e7
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2019-01-25 19:04:25 +0800

    Making it function!

--- src/app/index.js
+++ src/app/index.js
@@ -1,25 +1,17 @@
-import Generator from 'yeoman-generator';
-import fs from 'fs';
-import path from 'path';
+// import Generator from 'yeoman-generator';
+var Generator = require('yeoman-generator');
+// import fs from 'fs';
+// import path from 'path';
 import _ from 'lodash';
-import questions from './questions.js';
-import localGenerator from './generator';
+// import questions from './questions.js';
+// import localGenerator from './generator';
 
 export default class LegacyDeploymentGenerator extends Generator {
     constructor(args, opts) {
         super(args, opts);
-
-        // This makes `gen-path` a required argument.
-        // this.argument('gen_path', {
-        //     type: String,
-        //     required: true,
-        //     desc: 'A generation path for promised component'
-        // });
-
-        // fs.accessSync(this.options.gen_path, fs.constants.R_OK | fs.constants.W_OK);
     }
 
-    prompting() {
+    // prompting() {
         // return this.prompt(questions)
         //     .then((answers) => {
         //         var gen = new localGenerator(this.options.gen_path, answers);
@@ -30,5 +22,13 @@ export default class LegacyDeploymentGenerator extends Generator {
         //             }
         //         }
         //     });
+    // }
+
+    method1() {
+        this.log('method 1 just ran');
+    }
+
+    method2() {
+        this.log('method 2 just ran');
     }
 };
