f9c4932b5265f987ef0107ba7d8bfb0d12319756
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2019-02-15 18:19:40 +0800

    Some sensible questions added

--- generators/app/index.js
+++ generators/app/index.js
@@ -20,6 +20,44 @@ module.exports = class extends Generator {
         name: "someAnswer",
         message: "Would you like to enable this option?",
         default: true
+      },
+      {
+        name: "buildTool",
+        type: "list",
+        message: "Which tool you want to use to favour your build process?",
+        choices: ["GitLab", "TravisCI", "CicleCI"],
+        default: 1
+      },
+      {
+        name: "isRootAvailable",
+        type: "confirm",
+        message:
+          "We require a single server setup where multiple deployments would happen. For that we require sudo support, do you have it?",
+        default: true
+      },
+      {
+        name: "loginOption",
+        type: "list",
+        message: "What is a recommended way for you to connect to the server?",
+        choices: ["SSH-Key", "Username/Password pair"],
+        default: 1
+      },
+      {
+        name: "isUbuntuBased",
+        type: "confirm",
+        message:
+          "We recommend using Ubuntu based machine, and we only support it for the moment. Please confirm that you have it",
+        default: true
+      },
+      {
+        type: "input",
+        name: "numSteps",
+        message:
+          "Enter the numbers of steps you want to setup, later we will ask for each one",
+        default: 1,
+        validate: input => {
+          return isNaN(input) ? true : "Only numbers are allowed, retry?";
+        }
       }
     ];
 
